4f61dcff2c275df9f6f2e117cacfe3a539ef9b7c
Dasi012 <dastandulatbekuulu@gmail.com>
2024-01-06 21:27:34 +0600

    save deploy

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:`user-name`/`repositories-name`.git main:gh-pages
+# git push -f git@github.com:`Dasi012`/`New-Profile-Dastan`.git main:gh-pages
 
 cd -
 
